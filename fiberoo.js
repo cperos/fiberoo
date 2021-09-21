a9f9e3e621c9178431e2222d43bf3fbd9f8caa2e
Constantine Peros <89947281+cperos@users.noreply.github.com>
2021-09-21 10:00:07 -1000

    Update fiberoo.js

---
+++ fiberoo.js
@@ -0,0 +1,29 @@
+function fib() {
+  const fib0 = 0;
+  const fib1 = 1;
+  const arr = [];
+  arr[0] = fib0;
+  arr[1] = fib1;
+  for (let i = 2; i < 50; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr;
+}
+
+console.log(fib());
+
+const arr = [1, 4, 5, 6];
+
+function numsToStrings(arr1) {
+  return _.map(arr1, function (num) { return num.toString(); });
+}
+
+console.log(numsToStrings(arr));
+
+function numEvenNums(arr2) {
+  const evens = _.filter(arr2, function (num) { return num % 2 == 0; });
+  const sum = _.reduce(evens, function (memo, num) { return memo + num; }, 0);
+  return sum;
+}
+
+console.log(numsEvenNums(arr));
